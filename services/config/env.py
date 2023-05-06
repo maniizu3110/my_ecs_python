64aaca28940b4c8e99e4dde94307e1ed0c124fa4
maniizu3110 <maniizu3110@gmail.com>
2023-05-07 03:11:04 +0900

    change default service_name

--- services/config/env.py
+++ services/config/env.py
@@ -9,6 +9,7 @@ load_dotenv()
 class Env:
     def __init__(
         self,
+        service_name: str,
         aws_access_key_id: str,
         aws_secret_access_key: str,
         aws_region: str,
@@ -33,10 +34,9 @@ class Env:
         self.port: int = port
         self.build_path: str = build_path
         self.dockerfile_name: str = dockerfile_name
-        self.pascal_service_name: str = get_repository_name(
-            change_to_pascal_case(github_repository_url))+change_to_camel_case(env)
-        self.default_service_name: str = get_repository_name(
-            github_repository_url)+"-"+env
+        self.pascal_service_name: str = change_to_pascal_case(
+            service_name)+change_to_camel_case(env)
+        self.default_service_name: str = service_name+"-"+env
         self.vpc_cidr: str = vpc_cidr
         self.rds_allocated_storage: int = rds_allocated_storage
         self.rds_instance_type: str = rds_instance_type
