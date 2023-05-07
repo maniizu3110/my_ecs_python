e26185f2547a74b65851af6347c918dc1f06c7e8
maniizu3110 <maniizu3110@gmail.com>
2023-05-07 13:36:56 +0900

    add update

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# when error occurred, run cleanup function
+function cleanup {
+    python3 delete_workflow.py
+}
+
+# Activate the virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
+pip install -r requirements.txt
+npm install -g aws-cdk
+
+cdk bootstrap
+
+cdk deploy --require-approval never
+
+trap cleanup ERR
+
+# replace app .env with created aws resources information
+python3 update_env.py
+
+# create workflow and commit to github for build image to ecr
+python3 set_workflow.py --workflow-file .github/workflows/build_template.yaml
+python3 delete_workflow.py
+
+# update cdk
+cdk deploy --require-approval never
+
+python3 set_workflow.py --workflow-file .github/workflows/deploy_template.yaml
